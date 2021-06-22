ce562ee2e51cb495e7023dbf707e01216f1df9b7
Bouchemel Nasreddine <bouchemelnasreddine@gmail.com>
2021-06-22 20:03:01 +0100

    implementing history information

--- app/src/main/java/com/exemple/commee/fragments/profilefragments/ProfileHistoryFragment.java
+++ app/src/main/java/com/exemple/commee/fragments/profilefragments/ProfileHistoryFragment.java
@@ -10,7 +10,9 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.exemple.commee.R;
+import com.exemple.commee.activities.MainActivity;
 import com.exemple.commee.activities.Splash;
+import com.exemple.commee.fragments.ProfileFragment;
 
 public class ProfileHistoryFragment extends Fragment {
 
@@ -18,6 +20,14 @@ public class ProfileHistoryFragment extends Fragment {
         Splash.changeStatusBarColor(getActivity(), R.color.white);
         View view = inflater.inflate(R.layout.profile_history_fragment, container, false);
 
+        MainActivity.back.setVisibility(View.VISIBLE);
+        MainActivity.back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getFragmentManager().beginTransaction().setCustomAnimations(0, R.anim.slide_out_right).replace(R.id.frame_container, new ProfileFragment()).commit();
+                MainActivity.back.setVisibility(View.GONE);
+            }
+        });
         return view;
     }
 
